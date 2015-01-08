7a20c0ce68cee323aae930e2fcc1ce4ac3085ec0
Parker Zink <parker.zink@gmail.com>
2015-01-08 09:09:05 -0800

    Initial argument php files
    This is an argument php file that I am working on in class. It uses one language which is PHP.

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+function takes_array($input)
+{
+    echo "$input[0] + $input[1] = ", $input[0]+$input[1];
+}
+
+?>
+
+<?php
+function add_some_extra(&$string)
+{
+    $string .= 'a happy string.';
+}
+$str = 'Hello there! I am a string, ';
+add_some_extra($str);
+echo $str;    // outputs 'This is a string, and something extra.'
+?>
+
+<?php
+function mascot($type = "BULLDOGS!!!")
+{
+    return "HELLO WORLD! MY NAMES IS PARKER, AND I AM AT THE HEART OF THE $type.\n";
+}
+echo mascot();
+echo mascot(null);
+echo mascot("BULLDOGS!");
+?>
+
+
+<?php
+function school($place, $name = "PHS (Pasadena High School)")
+{
+    return "And where are they, you ask? Well, they are at none other than $name $place.\n";
+}
+ 
+echo school("- Parker Zink,"
+        . " Pasadena High School");   // works as expected
+?>
